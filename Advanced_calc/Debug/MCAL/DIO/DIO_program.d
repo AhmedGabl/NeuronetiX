8c0af15da04dc90b23adc7c6dc6a816567fea4ea
AhmedAbogabl <63268983+AhmedGabl@users.noreply.github.com>
2024-07-10 01:48:37 +0300

    Delete Advanced_calc directory

--- Advanced_calc/Debug/MCAL/DIO/DIO_program.d
+++
@@ -1,9 +0,0 @@
-MCAL/DIO/DIO_program.o MCAL/DIO/DIO_program.o: ../MCAL/DIO/DIO_program.c \
-  ../MCAL/DIO/DIO_interface.h ../MCAL/DIO/../../utils/BIT_MATH.h \
-  ../MCAL/DIO/../../utils/STD_TYPES.h
-
-../MCAL/DIO/DIO_interface.h:
-
-../MCAL/DIO/../../utils/BIT_MATH.h:
-
-../MCAL/DIO/../../utils/STD_TYPES.h:
